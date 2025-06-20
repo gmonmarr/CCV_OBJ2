0b675306b7d6468745d3694557b20c6ddc0f7905
Guillermo Montemayor <A01722402@tec.mx>
2025-06-20 12:51:02 -0600

    Script para inicializar repo y requirements txt para python

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Create folders
+mkdir archivos_apoyo
+mkdir datasets
+mkdir modelo_entrenado
+mkdir resultados
+
+REM Install Python dependencies
+pip install -r requirements.txt
---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Create folders: archivos_apoyo, datasets, modelo_entrenado, resultados
+mkdir -p archivos_apoyo datasets modelo_entrenado resultados
+
+# Pip requirements
+pip install -r requirements.txt
